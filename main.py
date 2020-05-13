8d2dce01e905c945bde7ba488a2caad77e499b93
Arsen Tsiurak <blizzcold1@gmail.com>
2020-05-13 03:51:12 +0300

    more ui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import sqlite3
+from design import Ui_MainWindow
 from PyQt5 import QtWidgets
 
 db = sqlite3.connect("hospital.db")
@@ -9,3 +10,10 @@ db.commit()
 class App(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+app = QtWidgets.QApplication(sys.argv)
+window = App()
+window.show()
+app.exec_()
